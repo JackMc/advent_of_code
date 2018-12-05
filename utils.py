98d1695317cd0fd57c60d512e5245c9d30e0795a
Jack McCracken <jack.mccracken@shopify.com>
2018-12-05 00:58:34 -0500

    Cache input to be a good boi

--- utils.py
+++ utils.py
@@ -1,8 +1,16 @@
 from requests import get
-from os.path import expanduser
+from os.path import expanduser, exists
 
-def getlines(challenge):
-    lines = get(f'https://adventofcode.com/2018/day/{challenge}/input', cookies={'session': open(expanduser('~/.aoc_session')).read().strip()}).text.split('\n')
+def getlines(challenge, debug=True):
+    cachepath = f'day{challenge}/input.txt'
+    if debug:
+        lines = open(f'day{challenge}/debug_input.txt').readlines()
+    if exists(cachepath):
+        lines = open(cachepath).readlines()
+    else:
+        input = get(f'https://adventofcode.com/2018/day/{challenge}/input', cookies={'session': open(expanduser('~/.aoc_session')).read().strip()}).text
+        lines = input.split('\n')
+        open(cachepath, 'w').write(input)
     if lines[-1] == '':
         return lines[:-1]
     return lines
